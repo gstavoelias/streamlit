e9538b5e0601c872267bb359f8d8ebc0a3cc3006
Gustavo Elias Cândido <86082917+gstavoelias@users.noreply.github.com>
2025-04-11 10:35:34 -0300

    manutencao pt1.

--- pages/MANUTENCAO.py
+++ pages/MANUTENCAO.py
@@ -9,15 +9,15 @@ with st.sidebar:
 
 
 st.title("Relatório de Falha das TCUs TECSCI")
-server = Server("http://127.0.0.1:8087/api/v1.0/")
-
+# server = Server("http://127.0.0.1:8087/api/v1.0/")
+# response = server.get_manutencao()
+# df = pd.json_normalize(response)
+df = pd.read_csv("manutencao.csv")
 
 st.header("Manutenções por dia")
-response = server.get_manutencao()
-df = pd.json_normalize(response)
 df["horario"] = pd.to_datetime(df["horario"])
 data = df.groupby(df["horario"].dt.date).size()
-bar_chart = px.bar(data, x=data.index, y=data.values,color_discrete_sequence=["#00C853"])
+bar_chart = px.bar(data, x=data.index, y=data.values,color_discrete_sequence=px.colors.sequential.Viridis_r[3:])
 bar_chart.update_traces(showlegend=False)
 bar_chart.update_layout(xaxis=dict(tickvals=list(data.index)))
 st.plotly_chart(bar_chart, use_container_width=True, theme="streamlit")
@@ -31,27 +31,34 @@ df["etapa_erro"] = df["rft.erro_id.etapa.nome"] + " – " + df["rft.erro_id.nome
 # Listar opções únicas para o menu
 opcoes = df["etapa_erro"].dropna().unique().tolist()
 opcoes.sort()
+opcoes.insert(0, "TODOS")
 selecionado = st.selectbox("Selecione o Tipo de Falha", opcoes)
 
-# Separar etapa e erro a partir do valor selecionado
-etapa_selecionada, falha_selecionada = selecionado.split(" – ", 1)
 
-# Filtrar o DataFrame com base em etapa e erro
-df_filtrado = df[
-    (df["rft.erro_id.etapa.nome"] == etapa_selecionada) &
-    (df["rft.erro_id.nome"] == falha_selecionada)
-]
+if selecionado == "TODOS":
+    df_filtrado = df
+else:
+    # Separar etapa e erro a partir do valor selecionado
+    etapa_selecionada, falha_selecionada = selecionado.split(" – ", 1)
+
+    # Filtrar o DataFrame com base em etapa e erro
+    df_filtrado = df[
+        (df["rft.erro_id.etapa.nome"] == etapa_selecionada) &
+        (df["rft.erro_id.nome"] == falha_selecionada)
+    ]
 
 # Agrupar soluções
 solucoes = df_filtrado["solucao.nome"].value_counts().reset_index()
 solucoes.columns = ["Solução", "Quantidade"]
 
 # Título e gráfico
-grafico_solucao = px.pie(solucoes, names="Solução", values="Quantidade", color_discrete_sequence=["#00C853"])
+grafico_solucao = px.pie(solucoes, names="Solução", values="Quantidade", color_discrete_sequence=px.colors.sequential.Viridis_r[3:])
+st.text(f"TOTAL: {len(df_filtrado)}")
 st.plotly_chart(grafico_solucao, use_container_width=True)
 
 st.header("Manutenções por Operador")
 por_operador = df["operador_id.nome"].value_counts().reset_index()
 por_operador.columns = ["Operador", "Quantidade"]
-chart = px.bar(por_operador, x="Operador", y="Quantidade", color_discrete_sequence=["#00C853"])
+chart = px.bar(por_operador, x="Operador", y="Quantidade", color_discrete_sequence=px.colors.sequential.Viridis_r[3:])
+st.text(f"TOTAL: {len(por_operador)}")
 st.plotly_chart(chart, use_container_width=True)
